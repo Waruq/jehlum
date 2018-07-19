7a6c5e20e15df17aa7e82e2b4179198f6a889505
tufail-bin-nazir <tufail.b.n@gmail.com>
2018-07-19 14:43:45 +0530

    firstcommit

---
+++ src/main/java/com/jehlum/JehlumApplication.java
@@ -0,0 +1,12 @@
+package com.jehlum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JehlumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JehlumApplication.class, args);
+	}
+}
