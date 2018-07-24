9541c8950978af3608806a33507e640a37fdfe06
waruq <teamwaruq@gmail.com>
2018-07-24 22:00:43 +0530

    proxy added

--- src/main/java/com/jehlum/controllers/RecievingNotificationController.java
+++ src/main/java/com/jehlum/controllers/RecievingNotificationController.java
@@ -1,6 +1,12 @@
 package com.jehlum.controllers;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.URL;
 import java.util.ArrayDeque;
 import java.util.Date;
 import java.util.Deque;
@@ -161,8 +167,23 @@ public class RecievingNotificationController {
 			Document doc;
 			
 			try {
+				 URL url = new URL("http://www.iustlive.com/Index/Default.aspx");
+				  Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("127.0.0.1", 8080)); // or whatever your proxy is
+				  HttpURLConnection uc = (HttpURLConnection)url.openConnection(proxy);
+
+				  uc.connect();
+				
 				System.err.println("conecting to islamic university");
-				doc = Jsoup.connect("http://www.iustlive.com/Index/Default.aspx").timeout(0).get();
+				 String line = null;
+				    StringBuffer tmp = new StringBuffer();
+				    BufferedReader in = new BufferedReader(new InputStreamReader(uc.getInputStream()));
+				    while ((line = in.readLine()) != null) {
+				      tmp.append(line);
+				    }
+
+				   doc = Jsoup.parse(String.valueOf(tmp));
+				
+				//doc = Jsoup.connect("http://www.iustlive.com/Index/Default.aspx").timeout(0).get();
 				Element content = doc.getElementById("quicktabs-container-quicktabs");
 				Elements links = content.getElementsByTag("a");
 				
